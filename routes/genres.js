6ec4bec1ab7d911cb7a0f1a6dbb974d9704a8038
tahirs95 <tahirs95@hotmail.com>
2021-03-17 18:13:02 +0500

    Code refactoring.

--- routes/genres.js
+++ routes/genres.js
@@ -1,5 +1,6 @@
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const validateObjectId = require('../middleware/validateObjectId');
 const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -41,7 +42,8 @@ router.delete('/:id', [auth, admin], async (req, res) => {
   res.send(genre);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
+
   const genre = await Genre.findById(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
